b76d5a4722e9be2a157173151d7842c7383bc24d
Mohammad Aktaruzzaman <aktaruzzamancse@gmail.com>
2024-08-18 09:43:28 +0600

    No Data Found

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { UserRouters } from "./app/modules/user/user.route";
 import { ServiceRouters } from "./app/modules/service/service.route";
 import { SlotRouters } from "./app/modules/slot/slot.route";
 import { BookingRouters } from "./app/modules/booking/booking.route";
+import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 
 //parsers
@@ -15,4 +16,8 @@ app.use("/api/auth", UserRouters);
 app.use("/api/services", ServiceRouters);
 app.use("/api/", SlotRouters);
 app.use("/api/", BookingRouters);
+
+//Not Found
+app.use(notFound);
+
 export default app;
